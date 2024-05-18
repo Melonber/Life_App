1bb53981e8578ccb1deb31cf913caedf85b66e9f
Melonber <melonber2@gmail.com>
2024-05-18 23:21:46 +0300

    numbers

--- WeekMatrixPage.js
+++ WeekMatrixPage.js
@@ -20,7 +20,7 @@ const WeekMatrixPage = ({ route }) => {
     const diffInMilliseconds = currentDate.getTime() - inputDate.getTime();
     const diffInWeeks = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24 * 7));
 
-    const matrix = Array.from({ length: 70 }, () =>
+    const matrix = Array.from({ length: 75 }, () =>
       Array.from({ length: 52 }, () => '🟩')
     );
 
@@ -37,31 +37,61 @@ const WeekMatrixPage = ({ route }) => {
 
   const matrix = generateMatrix(birthDate);
 
+  const getOrderNumber = (rowIndex, cellIndex) => {
+    const linearIndex = rowIndex * 52 + cellIndex + 1;
+    if ([1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50].includes(linearIndex)) {
+      return linearIndex;
+    }
+    return null;
+  };
+  
   const renderMatrix = () => {
     return matrix.map((row, rowIndex) => (
       <View key={rowIndex} style={styles.row}>
         {row.map((cell, cellIndex) => (
-          <Text key={cellIndex} style={styles.emoji}>{cell}</Text>
+          <View key={cellIndex}>
+            {getOrderNumber(rowIndex, cellIndex) && (
+              <Text style={styles.orderNumber}>{getOrderNumber(rowIndex, cellIndex)}</Text>
+            )}
+            <Text style={styles.emoji}>{cell}</Text>
+          </View>
         ))}
       </View>
     ));
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={styles.matrixContainer}>
-        {renderMatrix()}
+    <View style={styles.container}>
+      <Text style={styles.textVertical}>Age</Text>
+      <View style={styles.content}>
+        <Text style={styles.textHorizontal}>Weeks in 1 year</Text>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <View style={styles.matrixContainer}>
+            {renderMatrix()}
+          </View>
+        </ScrollView>
       </View>
-    </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+  },
   scrollContainer: {
     flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    top:-150
   },
+  
   matrixContainer: {
     borderWidth: 1,
     borderColor: 'black',
@@ -71,6 +101,23 @@ const styles = StyleSheet.create({
   },
   emoji: {
     fontSize: 5,
+  },
+  textVertical: {
+    transform: [{ rotate: '270deg' }],
+    position: 'absolute',
+    top: '50%',
+    left: 0,
+    marginTop: -250,
+  },
+  orderNumber: {
+    position: 'absolute',
+    fontSize: 5,
+    top: -15,
+    left: 0,
+  },
+  textHorizontal: {
+    textAlign: 'center',
+    top: 10, // Установите здесь 1 пиксель
   },
 });
 
